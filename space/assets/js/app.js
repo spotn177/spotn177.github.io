462214304aa63bc54ff47bfe22c5633e8ffb1f6e
Natalia <spotn177@newschool.edu>
2017-02-07 14:08:32 -0500

    clicking tutorial

---
+++ space/assets/js/app.js
@@ -0,0 +1,15 @@
+$( ".alphabet" ).draggable();
+
+var currentTemp = 35;
+$(".handle").click(function() {
+	currentTemp++;
+	$(".degrees").html(currentTemp);
+	$(".rotting").css("font-size",currentTemp);
+})
+
+$(".handle2").click(function() {
+	currentTemp--;
+	$(".degrees").html(currentTemp);
+		$(".rotting").css("font-size",currentTemp);
+
+})
