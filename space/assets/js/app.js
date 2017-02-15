6a8e5a855cbbca362d80869cce66c538e6526bdd
Natalia <spotn177@newschool.edu>
2017-02-15 12:38:03 -0500

    alphabet letters

--- space/assets/js/app.js
+++ space/assets/js/app.js
@@ -1,4 +1,35 @@
-$( ".alphabet" ).draggable();
+$( ".alphabet-o1" ).draggable();
+
+$( ".alphabet-p" ).draggable();
+
+$( ".alphabet-e1" ).draggable();
+
+$( ".alphabet-n" ).draggable();
+
+$( ".alphabet-t" ).draggable();
+
+$( ".alphabet-h" ).draggable();
+
+$( ".alphabet-e2" ).draggable();
+
+$( ".alphabet-d" ).draggable();
+
+$( ".alphabet-o2" ).draggable();
+
+$( ".alphabet-o3" ).draggable();
+
+$( ".alphabet-r" ).draggable();
+
+
+
+
+
+
+
+
+
+
+
 
 /*var currentTemp = 35;
 $(".handle").click(function() {
