2931db93d2d7af9d8b4e3def5bfaefa6180bcab7
Natalia <spotn177@newschool.edu>
2017-02-23 20:06:00 -0500

    more and more work

---
+++ space/uppage/assets/js/app.js
@@ -0,0 +1,46 @@
+$( ".alphabet-o1" ).draggable();
+
+$( ".alphabet-p" ).draggable();
+
+$( ".alphabet-e1" ).draggable();
+
+$( ".alphabet-n" ).draggable();
+
+$( ".alphabet-t" ).draggable();
+
+$( ".alphabet-h" ).draggable();
+
+$( ".alphabet-e2" ).draggable();
+
+$( ".alphabet-d" ).draggable();
+
+$( ".alphabet-o2" ).draggable();
+
+$( ".alphabet-o3" ).draggable();
+
+$( ".alphabet-r" ).draggable();
+
+
+
+
+
+
+
+
+
+
+
+
+/*var currentTemp = 35;
+$(".handle").click(function() {
+	currentTemp++;
+	$(".degrees").html(currentTemp);
+	$(".rotting").css("font-size",currentTemp);
+})
+
+$(".handle2").click(function() {
+	currentTemp--;
+	$(".degrees").html(currentTemp);
+		$(".rotting").css("font-size",currentTemp);
+
+})*/
