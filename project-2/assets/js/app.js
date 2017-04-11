793bd1c74a420d69cf9577fc0b14248433466c8e
Natalia <spotn177@newschool.edu>
2017-04-11 18:51:20 -0400

    brendans comments

--- project-2/assets/js/app.js
+++ project-2/assets/js/app.js
@@ -1,6 +1,4 @@
 
-$(function () {
-
   $(".wrapper2 > div").click(function () {
 
     var totalNum = $(".hidden > .letter-container").length;
@@ -8,7 +6,7 @@ $(function () {
     var min = 0;
     var max = totalNum - 1;
     var randomNum = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log(randomNum);
+    //console.log(randomNum);
     var $lb = $($(this).data("lb"));
 
     $lb.html("");
@@ -18,4 +16,41 @@ $(function () {
     else
       $(".slots").removeAttr("style");
   });
+
+$(".play1").click(function() {
+myStopFunction()
 });
+
+var playOne = setInterval(function(){ playColOne() }, 100);
+
+function playColOne() {
+         var totalNum = $(".hidden > .letter-container").length;
+
+    var min = 0;
+    var max = totalNum - 1;
+    var randomNum = Math.floor(Math.random() * (max - min + 1) + min);
+    //console.log(randomNum);
+    var $lb = $(".letter1-box");
+
+    $lb.html("");
+    $(".hidden > .letter-container").eq(randomNum).clone().appendTo($lb);
+    if ($(".letter1-box > div").attr("class") == $(".letter2-box > div").attr("class") && $(".letter2-box > div").attr("class") == $(".letter3-box > div").attr("class"))
+      $(".slots").css("background", "yellow");
+    else
+      $(".slots").removeAttr("style");
+}
+
+function myStopFunction() {
+    clearInterval(playOne);
+}
+
+
+  setInterval(
+    function(){ 
+      $(".play2").click();
+    }, 100);
+
+  setInterval(
+    function(){ 
+      $(".play3").click();
+    }, 100);
